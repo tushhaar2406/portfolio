a0b1885f199cf74ef0a5cb878a97210e6cf33a2b
Tushar Tambe <tushar24062001@gmail.com>
2025-03-10 22:38:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-(JS content here)
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener("click", function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute("href")).scrollIntoView({ behavior: "smooth" });
+        });
+    });
+});
